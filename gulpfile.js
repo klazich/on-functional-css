7b07810ae1e7525264bb971ac08b3b9715053c3d
Kevin Lazich <dev@kevinlazich.com>
2018-02-08 15:28:12 -0500

    chore: Update css task with solid css

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,14 @@ gulp.task('css', function () {
     let postcss = require('gulp-postcss')
     let sourcemaps = require('gulp-sourcemaps')
 
-    return gulp.src('src/**/*.css')
+    return gulp.src('src/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(postcss([
             require('postcss-import'),
+            require('postcss-nested'),
             require('postcss-cssnext'),
             require('cssnano')
-        ]))
+        ], { syntax: require('postcss-scss') }))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('public/'))
+        .pipe(gulp.dest('./public'))
 })
