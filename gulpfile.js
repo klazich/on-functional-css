a9e903d8042bc801b2278456ae82541b15ae26ca
Kevin Lazich <dev@kevinlazich.com>
2018-02-08 22:17:36 -0500

    refactor: Split gulp css task to css:sass and css:postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,35 @@
-let gulp = require('gulp')
+const gulp = require('gulp')
 
-gulp.task('css', function () {
-    let postcss = require('gulp-postcss')
-    let sourcemaps = require('gulp-sourcemaps')
+gulp.task('sass', function () {
+  let sass = require('gulp-sass')
 
-    return gulp.src('src/**/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(postcss([
-            require('postcss-import'),
-            require('postcss-nested'),
-            require('postcss-cssnext'),
-            require('cssnano')
-        ], { syntax: require('postcss-scss') }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./public'))
+  return gulp
+    .src('node_modules/bf-solid/_lib/solid.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('src/css'))
+})
+
+gulp.task('css:postcss', ['sass'], function () {
+  const postcss = require('gulp-postcss')
+  const sourcemaps = require('gulp-sourcemaps')
+
+  let options = {
+    uncss: {
+      html: ['public/index.html'],
+      ignore: [],
+    },
+  }
+
+  let processors = [
+    require('postcss-import'),
+    require('postcss-nested'),
+    require('postcss-cssnext'),
+  ]
+
+  return gulp
+    .src('src/css/styles.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss(processors))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('public/css'))
 })
