66b1a45efde43c3e2dc7188ee962dc4dbd17343b
Kevin Lazich <dev@kevinlazich.com>
2018-02-18 04:23:45 -0500

    feature: Add noop.js; a gulp plugin
    I was looking for a plugin like gulp-util's noop seeing as how
    gulp-util is now depreciated and couldn't find one. Gulp-util
    points you to Through2 to so I created a dead simple plugin
    that just passes the virtual file thingy down the pipe

---
+++ noop.js
@@ -0,0 +1,13 @@
+var through = require('through2')
+
+
+function NoopPlugin(file, encoding, done) {
+  this.push(file)
+  return done()
+}
+
+function noop() {
+  return through.obj(NoopPlugin)
+}
+
+module.exports = noop
