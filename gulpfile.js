02aec9cf44d4c86c47476b80bcaf740dedc8c4f9
Kevin Lazich <dev@kevinlazich.com>
2018-02-19 15:40:42 -0500

    chore: Finish up gulp refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,41 @@
-let gulp      = require('gulp')
-let newer     = require('gulp-newer')
-let imagemin  = require('gulp-imagemin')
+let gulp = require('gulp')
+let newer = require('gulp-newer')
+let imagemin = require('gulp-imagemin')
 let htmlclean = require('gulp-htmlclean')
-let cleanCSS  = require('gulp-clean-css')
-let postcss   = require('gulp-postcss')
-let rename    = require('gulp-rename')
-let Browser   = require('browser-sync')
-let noop      = require('./noop.js')
+let cleanCSS = require('gulp-clean-css')
+let postcss = require('gulp-postcss')
+let rename = require('gulp-rename')
+let sourcemaps = require('gulp-sourcemaps');
+let inject = require('gulp-inject-string')
+let Browser = require('browser-sync')
+let noop = require('./noop.js')
 
 let browser = Browser.create()
-let reload  = browser.reload
+let reload = browser.reload
 
+let options = {
+  uncss: { html: ['src/index.html'] },
+  fontMagician: {
+    display: 'fallback',
+    hosted: ['src/fonts/ttf', 'src/fonts/woff', 'src/fonts/woff2'],
+  },
+  cleanCSS: { format: 'beautify' },
+  rename: { suffix: '.min' },
+  htmlclean: {}
+}
 
-let fonts = (dest) => () =>
-  gulp.src('src/fonts/**/*')
+let fonts = dest => () =>
+  gulp
+    .src('src/fonts/**/*')
     .pipe(newer(dest))
     .pipe(gulp.dest(dest))
 
 gulp.task('fonts', fonts('tmp/fonts'))
 gulp.task('fonts:dist', fonts('dist/fonts'))
 
-
-let images = (dest) => () =>
-  gulp.src('src/images/**/*')
+let images = dest => () =>
+  gulp
+    .src('src/images/**/*')
     .pipe(newer(dest))
     .pipe(imagemin({ optimizationLevel: 5 }))
     .pipe(gulp.dest(dest))
@@ -30,51 +43,43 @@ let images = (dest) => () =>
 gulp.task('images', images('tmp/images'))
 gulp.task('images:dist', images('dist/images'))
 
-
-let html = (dest) => () =>
-  gulp.src('src/index.html')
+let html = dest => () =>
+  gulp
+    .src('src/index.html')
     .pipe(newer(dest))
-    .pipe(dest === 'dist' ? htmlclean() : noop())
+    .pipe(dest === 'dist' ? inject.replace('styles.css', 'styles.min.css') : noop())
+    .pipe(dest === 'dist' ? htmlclean(options.htmlclean) : noop())
     .pipe(gulp.dest(dest))
 
 gulp.task('html', ['images'], html('tmp'))
 gulp.task('html:dist', ['images:dist'], html('dist'))
 
-
-let css = (dest) => {
-  let options = {
-    uncss       : { html: ['src/index.html'] },
-    fontMagician: {
-      display: 'fallback',
-      hosted : ['src/fonts/ttf', 'src/fonts/woff', 'src/fonts/woff2'],
-    },
-    cleanCSS    : { format: 'beautify' },
-    assets      : { loadPaths: ['images/'] },
-    rename      : { suffix: '.min' },
-  }
+let css = dest => {
 
   let processors = [
     require('postcss-import'),
     require('postcss-nested'),
     require('postcss-cssnext'),
-    require('postcss-font-magician')(options.fontMagician)
+    require('postcss-font-magician')(options.fontMagician),
   ]
 
-  if ( dest === 'dist' ) {
+  if (dest === 'dist') {
     processors = [
       ...processors,
-      require('postcss-assets')(options.assets),
-      require('postcss-sorting'),
       require('postcss-uncss')(options.uncss),
+      require('postcss-sorting'),
       require('css-mqpacker'),
     ]
   }
 
   return () =>
-    gulp.src('src/styles/**/*')
+    gulp
+      .src('src/styles.css')
+      .pipe(sourcemaps.init())
       .pipe(postcss(processors))
-      // .pipe(dest === 'dist' ? rename(options.rename) : noop())
+      .pipe(dest === 'dist' ? rename(options.rename) : noop())
       .pipe(dest === 'dist' ? cleanCSS() : cleanCSS(options.cleanCSS))
+      .pipe(sourcemaps.write())
       .pipe(gulp.dest(dest))
       .pipe(browser.stream())
 }
@@ -82,21 +87,23 @@ let css = (dest) => {
 gulp.task('css', ['html', 'images', 'fonts'], css('tmp'))
 gulp.task('css:dist', ['html:dist', 'images:dist', 'fonts:dist'], css('dist'))
 
-
 gulp.task('build', ['html', 'css', 'fonts'])
 gulp.task('build:dist', ['html:dist', 'css:dist', 'fonts:dist'])
 
-
-gulp.task('browserSync', () => {
+gulp.task('browserSync', ['build'], () => {
   browser.init({
     server: { baseDir: 'tmp' },
   })
 })
 
-gulp.task('watch', ['build', 'browserSync'], () => {
+gulp.task('watch', ['build'], () => {
   gulp.watch('src/fonts/**/*', ['fonts'])
   gulp.watch('src/images/**/*', ['images'])
   gulp.watch('src/index.html', ['html'])
-  gulp.watch('src/styles/**/*.css', ['css'])
+  gulp.watch('src/styles.css', ['css'])
   gulp.watch('tmp/index.html').on('change', reload)
 })
+
+gulp.task('serve', ['watch', 'browserSync'])
+
+gulp.task('default', ['serve'])
