58663e43845528caa06d39862646d5acac976b18
Kevin Lazich <dev@kevinlazich.com>
2018-02-20 16:38:03 -0500

    feature: Add Vue.js to project
    Also webpack with babel is added to build pipeline to bundle the new js scripts.

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/js/index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'tmp')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  }
+};
