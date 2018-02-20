04aab82c1ad93fa1a2c9cc59c6875e0cd2adfc74
Kevin Lazich <dev@kevinlazich.com>
2018-02-20 16:40:07 -0500

    feature: Add scripts to inject code snippets

---
+++ src/js/blocks.js
@@ -0,0 +1,80 @@
+
+const data = {
+
+  block01: `/* Start of reusable styles here */`,
+
+  block02: `.homepage-promos .grid-50 {
+  width: 100%
+}
+.homepage-promos .grid-33 {
+  width: 100%
+}
+.homepage-promos .grid-34 {
+  width: 100%
+}`,
+
+  block03: `#seo-container {
+  display: none;
+}`,
+
+  block04: `.product-tab {
+height: 530px;
+width: 99.7%;
+}`,
+
+  block05: `.container-inner.promo-status {
+   float: center;
+}`,
+
+  block06: `.left {
+float: left!important;
+}`,
+
+  block07: `.left-max-scr1,
+.left-max-scr2,
+.left-max-scr3,
+.left-only-scr1 {
+  float: left;
+}`,
+
+  block08: `.left-min-scr2,
+.left-min-scr3,
+.left-max-scr3,
+.left-only-scr3 {
+  float: left;
+}`,
+
+  block09: `.header-nav-container .header-nav-list {
+  float: left;
+}
+
+.CA .header-nav-list.second {
+  float: left;
+}
+
+#nav.challenger-a .submenu-3col li,
+#nav.challenger-a .submenu-3col li {
+  float: left;
+}`,
+
+  block10: `.ie6 #footer-content .flex-control-nav li a,
+.ie7 #footer-content .flex-control-nav li a,
+.ie8 #footer-content .flex-control-nav li a {
+  float: left;
+}
+
+#nav.challenger-a li.menu-products {
+  float: left;
+}`,
+
+  block11: `.container-inner.promo-status`,
+
+  block12: `.red {
+color: #FF4136;
+}`,
+
+  block13: `&lt;div class“red”&gt;Some text&lt;/div&gt;`,
+}
+
+
+export default data
---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+
+import data from './blocks'
+
+
+let app = new Vue({
+  el: '#app',
+  data: data
+})
+
