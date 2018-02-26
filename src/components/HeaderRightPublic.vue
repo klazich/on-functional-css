9c11c411a8a43065a815ff6d801721452381db1a
Kevin Lazich <dev@kevinlazich.com>
2018-02-26 07:19:15 -0500

    feature: Move vue code to components dir

---
+++ src/components/HeaderRightPublic.vue
@@ -0,0 +1,21 @@
+<template>
+  <div key="public" class="inline-flex items-center ml-auto">
+    <a @click="$emit('toggled')"
+       href="#"
+       class="f6 link nowrap green mr3">Sign in</a>
+    <button type="button"
+            class="f6 link green pointer nowrap br2 ba b--green bg-white pv2 ph3 ml1 mv1">
+      Get started
+    </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'header-meta-public',
+  }
+</script>
+
+<style scoped>
+
+</style>
