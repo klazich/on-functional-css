e3a4c55f335d5361663ebb53d289af32c57cbeae
Kevin Lazich <dev@kevinlazich.com>
2018-02-26 16:13:59 -0500

    chore: Small tweeks

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,15 +1,10 @@
 <template>
-
   <div class="inline-flex items-center ml-auto">
-
-    <a @click="show = !show" href="#" class="link gray hover-black-80 mr2">
+    <a type="button" @click="show = !show;" class="link pointer gray hover-black-80 mr2">
       <SearchIcon/>
     </a>
-    <transition>
-      <input v-if="show" type="text" class="f6 w-100 b--none black-70 mr2" placeholder="Search">
-    </transition>
+    <input v-show="show" id="searchInput" type="text" class="f6 h2 w-100 b--none black-70 mr2" placeholder="Search">
   </div>
-
 </template>
 
 <script>
@@ -22,7 +17,7 @@
     },
     data() {
       return { show: false }
-    },
+    }
   }
 </script>
 
