9347f5cb2c82e03723703638eea15582ce5b6c18
Kevin Lazich <dev@kevinlazich.com>
2018-03-06 18:30:34 -0500

    refactor: Expand code snippet wrapping to be more readable

--- src/js/index.js
+++ src/js/index.js
@@ -11,7 +11,7 @@ const comp = (f, g) => x => f(g(x))
  * pin/unpin header with headroom.js
  */
 
- // grab an element
+// grab an element
 var myElement = document.querySelector("header");
 // construct an instance of Headroom, passing the element
 var headroom  = new Headroom(myElement, {
@@ -32,20 +32,22 @@ headroom.init();
  */
 
 let codeSnippets = [
-  '/* Start of reusable styles here */',
-  '.homepage-promos .grid-50 {\n    width: 100%\n}\n.homepage-promos .grid-33 {\n    width: 100%\n}\n.homepage-promos .grid-34 {\n    width: 100%\n}',
-  '#seo-container {\n    display: none;\n}',
-  '.product-tab {\n    height: 530px;\n    width: 99.7%;\n}',
-  '.container-inner.promo-status {\n    float: center;\n}',
-  '.left {\n    float: left!important;\n}',
-  '.left-max-scr1,\n.left-max-scr2,\n.left-max-scr3,\n.left-only-scr1 {\n    float: left;\n}',
-  '.left-min-scr2,\n.left-min-scr3,\n.left-max-scr3,\n.left-only-scr3 {\n    float: left;\n}',
-  '.header-nav-container .header-nav-list {\n    float: left;\n}\n\n.CA .header-nav-list.second {\n    float: left;\n}\n\n#nav.challenger-a .submenu-3col li,\n#nav.challenger-a .submenu-3col li {\n    float: left;\n}',
-  '.ie6 #footer-content .flex-control-nav li a,\n.ie7 #footer-content .flex-control-nav li a,\n.ie8 #footer-content .flex-control-nav li a {\n    float: left;\n}\n\n#nav.challenger-a li.menu-products {\n    float: left;\n}',
-  '.container-inner.promo-status',
-  '.red {\n    color: #FF4136;\n}',
-  '&lt;div class=“red”&gt;Some text&lt;/div&gt;',
-].map(text => `\n    <pre class="pa4 f7 f6-m f5-l overflow-x-auto"><code class="dark-gray lh-copy">${text}</code></pre>\n`)
+    '/* Start of reusable styles here */',
+    '.homepage-promos .grid-50 {\n    width: 100%\n}\n.homepage-promos .grid-33 {\n    width: 100%\n}\n.homepage-promos .grid-34 {\n    width: 100%\n}',
+    '#seo-container {\n    display: none;\n}',
+    '.product-tab {\n    height: 530px;\n    width: 99.7%;\n}',
+    '.container-inner.promo-status {\n    float: center;\n}',
+    '.left {\n    float: left!important;\n}',
+    '.left-max-scr1,\n.left-max-scr2,\n.left-max-scr3,\n.left-only-scr1 {\n    float: left;\n}',
+    '.left-min-scr2,\n.left-min-scr3,\n.left-max-scr3,\n.left-only-scr3 {\n    float: left;\n}',
+    '.header-nav-container .header-nav-list {\n    float: left;\n}\n\n.CA .header-nav-list.second {\n    float: left;\n}\n\n#nav.challenger-a .submenu-3col li,\n#nav.challenger-a .submenu-3col li {\n    float: left;\n}',
+    '.ie6 #footer-content .flex-control-nav li a,\n.ie7 #footer-content .flex-control-nav li a,\n.ie8 #footer-content .flex-control-nav li a {\n    float: left;\n}\n\n#nav.challenger-a li.menu-products {\n    float: left;\n}',
+    '.container-inner.promo-status',
+    '.red {\n    color: #FF4136;\n}',
+    '&lt;div class=“red”&gt;Some text&lt;/div&gt;',
+  ]
+  .map(text => `<code class="dark-gray lh-copy">${text}</code>`)
+  .map(text => `\n    <pre class="pa4 f7 f6-m f5-l overflow-x-auto">${text}</pre>\n`)
 
 let figureElements = document.querySelectorAll('figure[type=snippet]')
 
