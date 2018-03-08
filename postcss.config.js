a94b3429ef80197e6b2132a6456a392a27240126
Kevin Lazich <dev@kevinlazich.com>
2018-03-08 18:24:38 -0500

    chore: Reorder process execution

--- postcss.config.js
+++ postcss.config.js
@@ -4,14 +4,14 @@ const OUTDIR = ENV === 'production' ? 'docs' : 'dist'
 let config = {
   plugins: {
     'postcss-import'          : {},
-    'immutable-css'           : {}, 
+    'immutable-css'           : {},
     'postcss-nested'          : {},
     'postcss-cssnext'         : {},
     'postcss-svgo'            : {},
     'css-mqpacker'            : {},
     'postcss-sorting'         : ENV === 'production' ? {} : false,
+    'postcss-uncss'           : ENV === 'production' ? { html: [`${OUTDIR}/index.html`] } : false,
     'postcss-discard-comments': ENV === 'production' ? {} : false,
-    'postcss-uncss'           : ENV === 'production' ? { html: [`${OUTDIR}/index.html`], ignore: ['.*hover.*, .*focus.*'] } : false,
     'postcss-reporter'        : { clearMessages: true, throwError: false },
     'postcss-browser-reporter': ENV === 'production' ? false : {},
   },
