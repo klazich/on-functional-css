1379c5b66c1cccdf62408e8b1392cb777411e5ea
Kevin Lazich <dev@kevinlazich.com>
2018-03-10 14:12:25 -0500

    chore: Add ESlint to project

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "extends": "airbnb",
+    "parserOptions": {
+        "ecmaVersion": 8
+    },
+    "rules": {
+        "semi": "off",
+        "no-nested-ternary": "off",
+        "global-require": "off",
+    },
+    "env":{
+        "browser": true,
+        "node": true,
+    }
+};
