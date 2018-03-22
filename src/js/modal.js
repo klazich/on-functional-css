d528908b893d11391ee4fbbe260f26f5fb5ec327
Kevin Lazich <dev@kevinlazich.com>
2018-03-22 08:01:24 -0400

    chore: Small javascript changes

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,14 +2,16 @@
  * Modal Toggling
  */
 
-const scrollbarWidth = Math.floor(window.innerWidth - document.body.offsetWidth)
+const scrollbarWidth = Math.floor(window.outerWidth - window.innerWidth)
 
 const body = document.body
 const modal = document.querySelector('.js-modal')
 const wrap = document.querySelector('.js-wrap')
+const introBttn = document.querySelector('.js-intro-bttn')
 
 function onClick() {
   modal.classList.toggle('hide')
+  introBttn.classList.toggle('hide')
   body.classList.toggle('noscroll')
   wrap.classList.toggle('blur')
 
