5ad68fe8f196fce1050fdc2aaad583ac0245ea29
Kevin Lazich <dev@kevinlazich.com>
2018-03-23 02:09:58 -0400

    refactor: Finish javascript refactoring

--- src/js/headerPinning.js
+++ src/js/headerPinning.js
@@ -2,95 +2,24 @@
  * Header Pinning/Unpinning on Scroll
  */
 
+import throttle from 'lodash/throttle'
+
 const header = document.querySelector('header')
-
-const initial = {
-  ticking: false,
-  y: 0,
-  dy: 0,
-}
-
-const updateY = state => num => ({
-  ...state,
-  y: num ? num : state.y,
-  dy: num ? num - state.y : 0,
-})
-
-const setTicking = state => bool => ({
-  ...state,
-  ticking: bool,
-})
-
-const getDY = state => state.dy
-const isScrollUp = state => getDY(state) < 0
-const isScrollDown = state => getDY(state) > 0
-
-// ----------------------------------------------
-
-var latestKnownScrollY = 0,
-  ticking = false,
-  item = document.querySelectorAll('.item')
-
-function update(doStuff) {
-  // reset the tick so we can capture the next onScroll
-  ticking = false
-  doStuff()
-}
-
-function onScroll() {
-  latestKnownScrollY = window.scrollY
-  requestTick()
-}
-
-function requestTick() {
-  if (!ticking) {
-    requestAnimationFrame(update)
-  }
-  ticking = true
-}
-
-window.addEventListener('scroll', onScroll, false)
-
-// ---------------------------------------------
-
-function update(stuff) {
-  ticking = false // reset the tick so we can capture the next onScroll
-  stuff()
-}
-
-function onScroll() {
-  ticking = ticking || requestAnimationFrame(update)
-}
-
-window.addEventListener('scroll', onScroll, false)
-
-// ------------------------------------------------------
-
 let y = 0
 
-const onScroll = () => {
+function onScroll() {
   const dy = window.scrollY - y
 
-  if (dy > 0) header.style.transform = 'translateY(-200%)' // Δy > 0 -> scroll down -> hide header
-  if (dy < 0) header.style.transform = 'translateY(0%)' // Δy < 0 -> scroll up   -> show header
+  // Δy > 0 -> scroll down -> hide header
+  if (dy > 0) header.style.transform = 'translateY(-200%)'
+  // Δy < 0 -> scroll up -> show header
+  if (dy < 0) header.style.transform = 'translateY(0%)'
 
   y = window.scrollY
 }
 
-function throttle(handler) {
-  let ticking = false
-
-  return event => {
-    if (!ticking)
-      requestAnimationFrame(
-        // fires when screen has refreshed
-        () => {
-          ticking = false // reset flag
-          handler(event) // pass event to handler
-        }
-      )
-    ticking = true
-  }
+export default {
+  type: 'scroll',
+  handle: throttle(onScroll, 100),
+  triggers: [window],
 }
-
-window.addEventListener('scroll', throttle(onScroll.bind({ y: 0 })))
--- src/js/main.js
+++ src/js/main.js
@@ -3,20 +3,13 @@
  * https://github.com/timoxley/functional-javascript-workshop
  */
 
-import _ from 'lodash'
-
 import modal from './modal'
 import headerNav from './headerNav'
 import headerStyle from './headerStyle'
 import searchBar from './searchBar'
+import headerPinning from './headerPinning'
 
 /***** Add Listeners *********************************************************/
-
-// const addListeners = obj => {
-//   obj.triggers.forEach(elem => {
-//     elem.addEventListener(obj.type, obj.handle)
-//   })
-// }
 
 modal.triggers.forEach(elem => {
   elem.addEventListener(modal.type, modal.handle)
@@ -30,59 +23,6 @@ headerStyle.triggers.forEach(elem => {
 searchBar.triggers.forEach(elem => {
   elem.addEventListener(searchBar.type, searchBar.handle)
 })
-
-/***** Header pin/unpin on scrolling *****************************************/
-
-window.addEventListener('scroll', _.throttle(onScroll))
-window.addEventListener('scroll', throttle(onScroll))
-
-const header = document.querySelector('header')
-
-let aa = 0
-let bb = 0
-let y = 0
-
-function onScroll() {
-  const dy = window.scrollY - y
-  console.log(`${++aa} >>> ${window.scrollY}\t${y}\t${dy}`)
-
-  if (dy > 0) header.style.transform = 'translateY(-200%)' // Δy > 0 -> scroll down -> hide header
-  if (dy < 0) header.style.transform = 'translateY(0%)' // Δy < 0 -> scroll up   -> show header
-
-  y = window.scrollY
-}
-
-// function throttle(handler) {
-//   let ticking = false // to flag if painting
-
-//   return event => { // wait for next screen refresh
-//     if (!ticking) requestAnimationFrame(() => { // fired only when screen has refreshed
-//       console.log('update!')
-//       ticking = false // reset flag
-//       handler(event)  // pass event to handler
-//     })
-//     ticking = true
-//   }
-// }
-
-function throttle(handle) {
-  let painting = false // to flag if painting
-  let savedEvent // to keep track of the last scrollY
-
-  const runOnRepaint = () => {
-    // fired only when screen has refreshed
-    console.log(`${++bb} >>> Update!`)
-    painting = false // repaint is over
-    handle(savedEvent) // passed event to handle
-  }
-
-  return event => {
-    // the actual event handler
-    savedEvent = event // save our event at each call
-    if (!painting) {
-      // only if we weren't already doing it
-      painting = true // repainting is starting
-      requestAnimationFrame(runOnRepaint) // wait for next screen refresh
-    }
-  }
-}
+headerPinning.triggers.forEach(elem => {
+  elem.addEventListener(headerPinning.type, headerPinning.handle)
+})
