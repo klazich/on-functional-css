756d08835542117ee25ca92d818eb3d370928aee
Kevin Lazich <dev@kevinlazich.com>
2018-03-30 03:45:02 -0400

    refactor: Rename the js entry point.

---
+++ src/js/index.js
@@ -0,0 +1,27 @@
+/**
+ * js/index.js
+ */
+
+import modal from './modal'
+import headerNav from './headerNav'
+import headerStyle from './headerStyle'
+import searchBar from './searchBar'
+import headerPinning from './headerPinning'
+
+/***** Add Listeners ***************************/
+
+modal.triggers.forEach(elem => {
+  elem.addEventListener(modal.type, modal.handle)
+})
+headerNav.triggers.forEach(elem => {
+  elem.addEventListener(headerNav.type, headerNav.handle)
+})
+headerStyle.triggers.forEach(elem => {
+  elem.addEventListener(headerStyle.type, headerStyle.handle)
+})
+searchBar.triggers.forEach(elem => {
+  elem.addEventListener(searchBar.type, searchBar.handle)
+})
+headerPinning.triggers.forEach(elem => {
+  elem.addEventListener(headerPinning.type, headerPinning.handle)
+})
